c7303213c0375ed076c8350fc52846adf0b20649
vasterlord <vasterlord619@gmail.com>
2016-05-02 22:31:15 +0300

    Good now?

--- app/src/main/java/com/example/yulian/timerbench/MainActivity.java
+++ app/src/main/java/com/example/yulian/timerbench/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(" Sport&Fighting Timer");
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
